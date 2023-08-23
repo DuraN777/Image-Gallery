1d9109cb1bcc668cfacb19e39e4dc5f018a03e39
Djura Neskovic <neskovic991@gmail.com>
2023-08-23 22:25:59 +0200

    Download img on gallery icon click

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ function getImages(apiURL) {
 
 function generateHTML(images) {
   //Making <li> of all fetched images and adding them to existing <ul> gallery
-  console.log(images)
   imagesUl.innerHTML += images.map((img) => 
   `<li class="gallery__card" onclick="showLightbox('${img.photographer}', '${img.src.large2x}')">
     <img class="gallery__card-img" src="${img.src.large2x}" alt="${img.alt}">
@@ -46,7 +45,7 @@ function generateHTML(images) {
         <i class="fa-solid fa-camera"></i>
         <span class="gallery__card-photographer">${img.photographer}</span>
       </div>
-      <button class="gallery__card-btn">
+      <button class="gallery__card-btn" onclick="downloadImg('${img.src.large2x}');event.stopPropagation();">
         <i class="fa-solid fa-download"></i>
       </button>
     </div>
@@ -83,6 +82,22 @@ function hideLightbox() {
   lightBox.classList.remove("show");
   document.body.style.overflow = "auto";
 }
+
+function downloadImg (imgURL) {
+  // Converting img to blob, creating its download link, & donwloading it.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Blob
+  fetch(imgURL)
+  .then(res => res.blob())
+  .then(data => {
+    console.log(data);
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(data);
+    a.download = new Date().getTime(); // name for downloaded image
+    a.click();
+  })
+  .catch(err => console.log("Failed to download image! " + err));
+}
+
 
 getImages(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`);
 loadMoreBtn.addEventListener("click", loadMoreImages);
